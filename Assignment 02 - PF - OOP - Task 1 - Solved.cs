5b1b0a27f220adffd5c4217d4f868d43cabaa463
qasimamjad1 <138777997+qasimamjad1@users.noreply.github.com>
2023-07-12 09:47:00 +0500

    Update Assignment 02 - PF - OOP - Task 1 - Solved.cs
    Q1

--- Assignment 02 - PF - OOP - Task 1 - Solved.cs
+++ Assignment 02 - PF - OOP - Task 1 - Solved.cs
@@ -1,9 +1,41 @@
+
 using System;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int[] numbers = { 2, 5, 7, 10, 12, 15, 18, 20, 21, 24 };
+        int largestOdd = FindLargestOdd(numbers);
+        int largestEven = FindLargestEven(numbers);
+
+        int sum = largestOdd + largestEven;
+        Console.WriteLine("Sum of largest odd and largest even: " + sum);
+    }
+
+    static int FindLargestOdd(int[] numbers)
+    {
+        int largestOdd = int.MinValue;
+        foreach (int number in numbers)
+        {
+            if (number % 2 != 0 && number > largestOdd)
+            {
+                largestOdd = number;
+            }
+        }
+        return largestOdd;
+    }
+
+    static int FindLargestEven(int[] numbers)
+    {
+        int largestEven = int.MinValue;
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0 && number > largestEven)
+            {
+                largestEven = number;
+            }
+        }
+        return largestEven;
     }
 }
